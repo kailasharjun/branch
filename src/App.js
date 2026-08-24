3e4c763343064c55e686cb1d8170fa2d4cd78c70
kailasharjun <kailashnaidu980@gmail.com>
2026-08-24 11:20:23 +0530

    Add React project files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React,{useState} from 'react';
+const Counter=()=> {
+    const [count,setCount]=useState(0);
+    const incrementCounter=()=> {
+        setCount(c=>c+1);
+    };
+    return(
+        <div className="counter-container">
+            <div className="counter-box">
+                <h1>Counter: {count}</h1>
+                <button className="counter-button"onClick={incrementCounter}>
+                    Increase Counter
+                    </button>
+                    </div>
+                    </div>
+    );
+};
+export default Counter;
